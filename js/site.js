3870541115f829201915105e986bf249566c2245
Florencia <floralemanpesce@gmail.com>
2021-07-08 15:37:39 -0300

    commit 4

---
+++ js/site.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".ul")
+
+document.querySelector(".navbar-toggler").addEventListener("click", e => {
+e.preventDefault()
+
+if (ul.classList.contains("open")){
+  ul.classList.remove("open")
+} else{
+  ul.classList.add("open")
+}
+
+})
